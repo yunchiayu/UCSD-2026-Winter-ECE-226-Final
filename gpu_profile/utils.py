2b34dcfb8bddc207ff1c8f3bf37092ef9c0708f7
yunchiayu <yunchiayu1206@gmail.com>
2026-03-18 16:19:19 -0700

    fixed error when tokenizer == None

--- gpu_profile/utils.py
+++ gpu_profile/utils.py
@@ -1,7 +1,10 @@
 import torch
 def get_fake_prompt(tokenizer, batch_size: int, seq_len: int, device: str = "cuda"):
     # avoid special tokens (usually <10 ids)
-    vocab_size = tokenizer.vocab_size
+    if tokenizer != None:
+        vocab_size = tokenizer.vocab_size
+    else:
+        vocab_size = 5000
 
     min_token_id = 10
     max_token_id = vocab_size - 1
